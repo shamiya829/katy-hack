19c9cd3e670943235e5ea452fad9783891f7d531
Shamiya Lin <shamiya2023@gmail.com>
2022-02-05 22:59:55 -0600

    changed meta and site title

--- lib/constants.ts
+++ lib/constants.ts
@@ -16,17 +16,17 @@
 
 export const SITE_URL = 'https://demo.vercel.events';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
-export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const TWITTER_USER_NAME = 'gwckaty';
+export const BRAND_NAME = 'Katy Hack';
+export const SITE_NAME_MULTILINE = ['GWC', 'Hack'];
+export const SITE_NAME = 'Katy Conf';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+  'This is a hackthon hosted by Girls Who Code Katy.';
 export const SITE_DESCRIPTION =
-  'An interactive online experience by the community, free for everyone.';
+  'An interactive online experience by the community, free for everyone, brought to you by Girls Who Code Katy.';
 export const DATE = 'October 27, 2020';
-export const SHORT_DATE = 'Oct 27 - 9:00am PST';
-export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
+export const SHORT_DATE = 'Apr 12 - 9:00am CST';
+export const FULL_DATE = 'April 27th 9am Central Time';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
