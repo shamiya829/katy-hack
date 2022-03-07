e36e1f8eb76eff110a1ba309b6d1e0f0fc6dd8a0
Shamiya Lin <shamiya2023@gmail.com>
2022-03-06 21:43:18 -0600

    change icons

--- components/icons/icon-logo.tsx
+++ components/icons/icon-logo.tsx
@@ -25,7 +25,7 @@ export default function IconLogo({
       height="100%"
       viewBox="0 0 32 32"
       fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+      xmlns="public/favicon.ico"
       {...props}
     >
       <rect width="100%" height="100%" rx="16" fill={backgroundColor} />
